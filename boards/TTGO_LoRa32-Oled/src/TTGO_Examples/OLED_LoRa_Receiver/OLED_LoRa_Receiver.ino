0dc711b41a569c343888c375c254a45ea8ee1931
Stefan Fambach <spam1@bobbelsche.de>
2021-01-26 18:48:25 +0100

    first

---
+++ boards/TTGO_LoRa32-Oled/src/TTGO_Examples/OLED_LoRa_Receiver/OLED_LoRa_Receiver.ino
@@ -0,0 +1,84 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include <SSD1306.h>
+#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    433E6
+
+#define DISPLAY_SDA 4
+#define DISPLAY_SCL 15
+#define DISPLAY_RST 16
+#define SSD_ADDRESS 0x3c
+
+SSD1306 display(SSD_ADDRESS, DISPLAY_SDA, DISPLAY_SCL);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+
+void setupDisplay(){
+    // Init display 
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+
+  display.init();
+  display.flipScreenVertically();  
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(20, 20, "Display Setup finished");
+  display.display();
+  delay(1500);
+}
+
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 26 , 128, packet);
+  display.drawString(0, 0, rssi);  
+  display.display();
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+void setup() {
+
+  setupDisplay();
+    
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Receiver Callback");
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+  Serial.println("init ok");
+
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
+}
---
+++ boards/TTGO_LoRa32-Oled/src/TTGO_Examples/OLED_LoRa_Sender/OLED_LoRa_Sender.ino
@@ -0,0 +1,89 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    433E6
+
+unsigned int counter = 0;
+
+#define DISPLAY_SDA 4
+#define DISPLAY_SCL 15
+#define DISPLAY_RST 16
+#define SSD_ADDRESS 0x3c
+
+SSD1306 display(SSD_ADDRESS, DISPLAY_SDA, DISPLAY_SCL);
+
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+
+void setupDisplay(){
+    // Init display 
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+
+  display.init();
+  display.flipScreenVertically();  
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(20, 20, "Display Setup finished");
+  display.display();
+  delay(1500);
+}
+
+
+void setup() {
+
+
+  setupDisplay();
+  
+  pinMode(25,OUTPUT);
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Sender Test");
+  
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+//  LoRa.receive();
+  Serial.println("init ok");
+  delay(1500);
+}
+
+void loop() {
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  
+  display.drawString(0, 0, "Sending packet: ");
+  display.drawString(90, 0, String(counter));
+  display.display();
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
