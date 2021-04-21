c2ae745f15bf5adbb508cb81151c765c65add60e
Stefan Fambach <spam1@bobbelsche.de>
2021-04-21 19:27:24 +0200

    New Mqtt

---
+++ boards/HighGrow/src/HiGrowESP32MQTT/WIFI.h
@@ -0,0 +1,39 @@
+#ifndef MY_WIFI_H
+#define MY_WIFI_H
+
+#include <WiFi.h>
+#include "debug.h"
+#include "credentials.h"
+WiFiClient espClient;
+
+void wifiConnect()
+{
+  int count = 0;
+  DEBUG_PRINT("[WIFI] Connecting to ");
+  DEBUG_PRINT(mySSID);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    WiFi.begin(mySSID, myPW);
+    delay(1000);
+    DEBUG_PRINT(".");
+    count++;
+    if (count > 20) ESP.restart();
+  }
+  
+  DEBUG_PRINTLN("[WIFI] connected");
+  DEBUG_PRINT("[WIFI] IP address: ");
+  DEBUG_PRINTLN(WiFi.localIP());
+}
+
+void wifiDisconnect(){
+  DEBUG_PRINTLN("[WIFI] Going to sleep...");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  btStop();
+  
+}
+
+#endif // #define MY_WIFI_H
